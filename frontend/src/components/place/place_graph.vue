352e8ea604899146a85e1859926f676ce09cdb36
Yuan Chen <jerrych.97@gmail.com>
2017-02-28 21:19:46 -0500

    add chart element

--- frontend/src/components/place/place_graph.vue
+++ frontend/src/components/place/place_graph.vue
@@ -1,11 +1,36 @@
 <template lang="html">
 <div>
 <h1 class="title">Hours</h1>
+<canvas id="hoursChart"></canvas>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'place_graph'
+  name: 'place_graph',
+  props: ['chart_data'],
+  data: function () {
+    return {
+      data: function () {
+        return chart_data;
+      }
+    }
+  },
+  watch: {
+    chart_data: function (new_data) {
+      this.data = new_data;
+      /* watches for whenever data changes */
+    }
+  },
+  methods: {
+
+  } 
 }
 </script>
+
+<style type="text/css">
+#hoursChart {
+  width: 100%;
+  height: 800px;
+}
+</style>
