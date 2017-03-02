a584e2e9060ca376b47f8f2f7d3b52d1f5211b2b
amccannv <amccan5@uwo.ca>
2017-03-02 17:30:14 -0500

    Revert to 274be1ac4aa130a78a58b181755f1be777bce12d

--- Capacity/grails-app/controllers/capacity/PlaceController.groovy
+++ Capacity/grails-app/controllers/capacity/PlaceController.groovy
@@ -22,8 +22,8 @@ class PlaceController extends RestfulController{
     def getPlaceInfo() {
         // Begin place retrieval..
         System.out.println('Request for place data recieved.\n')
-        def gID = params.googleID
-        def place = Place.find{googleID == gID}
+        def googleID = params.googleID
+        def place = Place.find{googleID == googleID}
 
         // If place exists, retrieve data.
         if(place != null) {
@@ -36,16 +36,13 @@ class PlaceController extends RestfulController{
         // new place, create place object and respond with data
         else {
             System.out.print('New place needs to be created. ')
-            place = new Place(googleID: gID)
-            place.save()
+            place = new Place(googleID: googleID)
             def jsonPlace = {
-                def result = [location: place]
                 render place as JSON
             }
-            System.out.print(place.googleID)
-            System.out.print('\nNew place created.\n')
+            System.out.print(jsonPlace.toString())
+            System.out.print('New place created.\n')
             response.status = 200
         }
     }
-
 }
