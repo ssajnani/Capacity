e9e4505dbf3af30cf3d5bd9765c6f3d6bdadcfd6
steven <scrossma@uwo.ca>
2017-03-02 11:16:53 -0500

    First Message Board

--- Capacity/grails-app/domain/capacity/MessageBoard.groovy
+++ Capacity/grails-app/domain/capacity/MessageBoard.groovy
@@ -1,9 +1,19 @@
-package capacity
+package Capacity
 
 class MessageBoard {
+    def messageMap = [:]
+    def idList = []
 
     static hasMany = [messages : Message]
 
-    static constraints = {
+    def getID(int arrayCount){ //gets the id specified by the parameter
+        return idList.get(arrayCount)
+    }
+    def messageMapping(Message newMessage){ //Stores new messages in a map
+        messageMap.put(newMessage.getMessageID, newMessage.getText)
+        idList >> newMessage.getMessageID
+    }
+    def retrieveMessage(int idNumber){ //Retrieves a message corresponding to an id number from the map
+        messageMap.get(idNumber)
     }
 }
