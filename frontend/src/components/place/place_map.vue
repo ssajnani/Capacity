6ab15c1d6db3d9c880337a3a8dce83eac47d1e57
VictoryShoe <victoryshoe@hotmail.com>
2017-03-20 21:58:37 -0400

    Added heatmap

--- frontend/src/components/place/place_map.vue
+++ frontend/src/components/place/place_map.vue
@@ -2,9 +2,27 @@
 
 <!-- Google Maps -->
 <div class="card-image">
+  <div id="filters-panel">
+    <a class="button" v-on:click="toggleHeatmap()">Toggle Heatmap</a>
+    <a class="button" v-on:click="toggleTraffic()">Traffic</a>
+    <a class="button" v-on:click="toggleTransit()">Transit</a>
+    <a class="button" v-on:click="toggleBicycle()">Bicycle</a>
+  </div>
   <div id="map"></div>
 </div>
 </template>
+
+<style>
+  #map {
+    height: 400px;
+    width: 100%;
+  }
+  #filters-panel {
+    position: absolute;
+    z-index: 5;
+    padding: 1%;
+  }
+</style>
 
 <script>
 export default {
@@ -12,26 +30,85 @@ export default {
   props: ['coords', 'suggestions'],
   data: function () {
     return {
-      // location: function () {
-      //   return this.coords;
-      // }
+      map: null,
+      heatmap: null,
+      trafficLayer: null,
+      transitLayer: null,
+      bikeLayer: null
     };
   },
   methods: {
+    // var dataArray = [
+    //     [43.0082, -81.2606, new Date()],
+    //     [43.0115, -81.2793, new Date()],
+    //     [43.000286, -81.278336, new Date()],
+    //     [43.0022, -81.3000, new Date()]];
+       
+    // var todayArray = [];
+    // var weekArray = [];
+    // var monthArray = [];
+
+    // var i = 0;
+
+    // for (i = 0; i < dataArray.length; i++) {
+    //     var long = dataArray[i][0];
+    //     var lat =  dataArray[i][1]
+    //     var time = dataArray[i][2];
+
+    //    var otherDate = new Date(time);
+    //     var otherDay = otherDate.getDay();
+    //     var otherMonth = otherDate.getMonth();
+    //     var otherYear = otherDate.getFullYear();
+
+
+    //    var currentDate = new Date();
+    //     var day = currentDate.getDay();
+    //     var month = currentDate.getMonth();
+    //     var year = currentDate.getFullYear();
+
+    //    // today, week, month
+
+    //    if (day == otherDay && year == otherYear && month == otherMonth) { // today
+    //         todayArray.push({long, lat});
+    //     }
+
+    //    if (month == otherMonth && year == otherYear) { // month
+    //         monthArray.push({long, lat});
+    //     }
+        
+    //     var diffDate = Math.abs(otherDate - currentDate);
+    //     var diffDays = (diffDate / 1000) / 60 / 60 / 24;
+    //     if (diffDays <= 7 && year == otherYear && month == otherMonth) { // week
+    //         weekArray.push({long, lat});
+    //     }
+    // }
+
+    // console.log(todayArray);
+    // console.log(monthArray);
+    // console.log(weekArray);
+
+    toggleHeatmap: function() {
+      this.heatmap.setMap(this.heatmap.getMap() ? null : this.map);
+    },
+    toggleTraffic: function () {
+      this.trafficLayer.setMap(this.trafficLayer.getMap() ? null : this.map);
+    },
+    toggleTransit: function () {
+      this.transitLayer.setMap(this.transitLayer.getMap() ? null : this.map);
+    },
+    toggleBicycle: function () {
+      this.bikeLayer.setMap(this.bikeLayer.getMap() ? null : this.map);
+    }, 
     initMap: function() {
-
-      // Placeholder for searched item
-      // Coords of UWO 43.0096° N, 81.2737° W
       this.location = this.coords;
-
+      // Google Map options
       this.map = new google.maps.Map(document.getElementById('map'), {
         center: this.location,
-        // Google Map options, no changing to satellite or street view
         mapTypeControl: false,
-        streetViewControl: false,
-        zoom: 12,
-        maxZoom: 17,
-        minZoom: 11,
+        // streetViewControl: false,
+        zoom: 14,
+        maxZoom: 16,
+        minZoom: 12,
         // Styling for the Google Map, can be changed as necessary
         styles: [
           {
@@ -39,7 +116,8 @@ export default {
             "elementType": "all",
             "stylers": [
               {
-                "visibility": "off"
+                // "visibility": "off"
+                "visibility": "on"
               }
             ]
           },
@@ -75,7 +153,8 @@ export default {
             "elementType": "all",
             "stylers": [
               {
-                "visibility": "simplified"
+                // "visibility": "simplified"
+                "visibility": "on"
               }
             ]
           },
@@ -111,7 +190,8 @@ export default {
             "elementType": "all",
             "stylers": [
               {
-                "visibility": "simplified"
+                // "visibility": "simplified"
+                "visibility": "on"
               }
             ]
           },
@@ -120,7 +200,8 @@ export default {
             "elementType": "all",
             "stylers": [
               {
-                "visibility": "simplified"
+                // "visibility": "simplified"
+                "visibility": "on"
               }
             ]
           },
@@ -129,7 +210,8 @@ export default {
             "elementType": "all",
             "stylers": [
               {
-                "visibility": "simplified"
+                // "visibility": "simplified"
+                "visibility": "on"
               }
             ]
           },
@@ -154,25 +236,25 @@ export default {
         ]
       });
 
-      // Custom marker icons
-      // var mainMarker = 'https://image.flaticon.com/icons/png/128/179/179386.png'
-      // var suggestedMarker = 'http://findicons.com/files/icons/1963/colorcons_blue/128/questionmark.png';
+      // Custom marker icons code
+        // var mainMarker = 'https://image.flaticon.com/icons/png/128/179/179386.png'
+        // var suggestedMarker = 'http://findicons.com/files/icons/1963/colorcons_blue/128/questionmark.png';
 
-      // Marker sizes are expressed as a Size of X,Y where the origin of the image
-      // (0,0) is located in the top left of the image.
-      // Origins, anchor positions and coordinates of the marker increase in the X
-      // direction to the right and in the Y direction down.
-      // var image = {
-      //   url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-      //   // This marker is 20 pixels wide by 32 pixels high.
-      //   size: new google.maps.Size(20, 32),
-      //   // The origin for this image is (0, 0).
-      //   origin: new google.maps.Point(0, 0),
-      //   // The anchor for this image is the base of the flagpole at (0, 32).
-      //   anchor: new google.maps.Point(0, 32)
-      // };
+        // Marker sizes are expressed as a Size of X,Y where the origin of the image
+        // (0,0) is located in the top left of the image.
+        // Origins, anchor positions and coordinates of the marker increase in the X
+        // direction to the right and in the Y direction down.
+        // var image = {
+        //   url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+        //   // This marker is 20 pixels wide by 32 pixels high.
+        //   size: new google.maps.Size(20, 32),
+        //   // The origin for this image is (0, 0).
+        //   origin: new google.maps.Point(0, 0),
+        //   // The anchor for this image is the base of the flagpole at (0, 32).
+        //   anchor: new google.maps.Point(0, 32)
+        // };
 
-      // Main marker dropped
+      // Main marker dropping
       var mainMarker = new google.maps.Marker({
         position: this.location,
         map: this.map,
@@ -181,34 +263,58 @@ export default {
       });
 
       // More placeholders for marker drop, represents suggested locations
-      // King Richie's 43.0082° N, 81.2606° W
-      // Saugeen-Maitland Hall 43.0115° N, 81.2793° W
-      // 7/11 43.0016° N, 81.2768° W
-      // var suggestedLocations = [
-      //   {lat: 43.0082, lng: -81.2606},
-      //   {lat: 43.0115, lng: -81.2793},
-      //   {lat: 43.0016, lng: -81.2768}
-      // ];
+        // King Richie's 43.0082° N, 81.2606° W
+        // Saugeen-Maitland Hall 43.0115° N, 81.2793° W
+        // 7/11 43.0016° N, 81.2768° W
+        // var suggestedLocations = [
+        //   {lat: 43.0082, lng: -81.2606},
+        //   {lat: 43.0115, lng: -81.2793},
+        //   {lat: 43.0016, lng: -81.2768}
+        // ];
 
-      // var labels = '123456789';
-      // var labelIndex = 0; 
+      var labels = '123456789';
+      var labelIndex = 0; 
 
-      // function addMarkerWithTimeout(location, map, timeout) {
-      //   setTimeout(function(){ 
-      //     var newMarker = new google.maps.Marker({
-      //       position: location,
-      //       map: map,
-      //       animation: google.maps.Animation.DROP,
-      //       // label: labels[labelIndex++ % labels.length],
-      //       // icon: suggestedMarker
-      //     });
-      //   }, timeout);
-      // };
+      function addMarkerWithTimeout(location, map, timeout) {
+        setTimeout(function(){ 
+          var newMarker = new google.maps.Marker({
+            position: location,
+            map: map,
+            animation: google.maps.Animation.DROP,
+            // label: labels[labelIndex++ % labels.length],
+            // icon: suggestedMarker
+          });
+        }, timeout);
+      };
 
       // Wierd i values to adjust for drop time against mainMarker
       // for (var i = 1; i <= suggestedLocations.length; i++) {
       //   addMarkerWithTimeout(suggestedLocations[i-1], this.map, i * 200);
       // };
+
+      // Dummy map data
+      var heatmapData = [
+        {location: new google.maps.LatLng(43.0082, -81.2606), weight: 5},
+        {location: new google.maps.LatLng(43.0115, -81.2793), weight: 10},
+        {location: new google.maps.LatLng(43.000286, -81.278336), weight: 3},
+        {location: new google.maps.LatLng(43.000735, -81.276809), weight: 5},
+
+        new google.maps.LatLng(43.0115, -81.2793),
+        new google.maps.LatLng(43.0016, -81.2768),
+        new google.maps.LatLng(43.001623, -81.276882),
+      ];
+
+      // Initialize heatmap and other layers
+      this.heatmap = new google.maps.visualization.HeatmapLayer({
+        data: heatmapData
+      });
+      this.trafficLayer = new google.maps.TrafficLayer();
+      this.transitLayer = new google.maps.TransitLayer();
+      this.bikeLayer = new google.maps.BicyclingLayer();
+
+      // Options for heatmap
+        this.heatmap.set('radius', 30);
+        this.heatmap.set('dissipating', true);
     }
   },
   watch: {
@@ -220,10 +326,3 @@ export default {
   }
 }
 </script>
-
-<style>
-  #map {
-    height: 400px;
-    width: 100%;
-  }
-</style>
